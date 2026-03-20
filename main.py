76a2cc833755aaadd6a70bf06f375a68d9e518ae
noahmasri <mmasri@fi.uba.ar>
2026-03-20 11:03:30 +0100

    add algorithm exec time metric

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from json_parser import load_tasks_from_json, save_results_to_json
 from ods_soea import get_ods_scheduling
 from test_ods_soea import draw_scheduling_gantt
 from visualizer import draw_task_dag
+import time
 
 def main():
     if len(sys.argv) < 2:
@@ -15,14 +16,17 @@ def main():
 
     # Load tasks from JSON
     tasks = load_tasks_from_json("task_graph.json", processors)
+    start = time.time()
     allocation, finish_times = get_ods_scheduling(tasks, processors, l_idx=len(tasks) - 1)
+    end = time.time()
+    print(f"Algorithm execution time: {end - start:.8f} seconds")
 
     # Dump results to JSON
     save_results_to_json(allocation, finish_times)
 
     # Save Gantt chart
-    draw_task_dag(tasks, output_path="graph.png")
-    draw_scheduling_gantt(allocation, finish_times, tasks, processors, output_path="gantt.png")
+    draw_task_dag(tasks, output_path="graph.png", task_prefix="")
+    draw_scheduling_gantt(allocation, finish_times, tasks, processors, output_path="gantt.png", task_prefix="")
 
 if __name__ == "__main__":
     main()
